460877a542da2d6c529ab2df772c220cd55796a9
jesus villarreal <jv5655217@gmail.com>
2025-10-31 07:59:03 -0500

    Inicial commit: backend de Tank con NestJS y Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { TanksModule } from './tanks/tanks.module';
 import { MqttModule } from './mqtt/mqtt.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TanksModule, MqttModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),TanksModule, MqttModule],
   providers: [PrismaService],
 })
 export class AppModule {}
