607a4660c5ea5e7911027dce428e266083d40466
jesus villarreal <jv5655217@gmail.com>
2025-10-31 20:28:22 -0500

    Fix: ensure tsconfig.json copied for NestJS build

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
   
   // 👇 Esto es clave para que funcione en Docker
-  await app.listen(port, '0.0.0.0');
+   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
-  console.log(`🚀 API corriendo en http://0.0.0.0:${port}`);
+  console.log(`🚀 API corriendo en http://0.0.0.0:${process.env.PORT}`);
 }
 bootstrap();
