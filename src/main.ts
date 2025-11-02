6533b2ff126f07cfee56d8d87533f84bc630426c
jesus villarreal <jv5655217@gmail.com>
2025-11-01 19:52:56 -0500

    Inicial commit: backend de Tank con NestJS y Docker

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  // CRÍTICO: Usar una variable para el puerto.
+  const port = process.env.PORT || 3000;
   
-  // 👇 Esto es clave para que funcione en Docker
-   await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  // Escuchar en el puerto detectado y en todas las interfaces ('0.0.0.0')
+  await app.listen(port, '0.0.0.0');
 
-  console.log(`🚀 API corriendo en http://0.0.0.0:${process.env.PORT}`);
+  // Ahora el log siempre mostrará el puerto correcto (3000 local, 8080 en Railway)
+  console.log(`🚀 API corriendo en http://0.0.0.0:${port}`);
 }
 bootstrap();
